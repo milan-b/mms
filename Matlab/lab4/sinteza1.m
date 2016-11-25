9c8abe3b0978be007a8638be02371e9aa59dd317
Milan B <milanboji@gmail.com>
2016-11-25 11:58:44 +0100

    Peti lab

--- Matlab/lab4/sinteza1.m
+++ Matlab/lab4/sinteza1.m
@@ -2,8 +2,10 @@ function label = sinteza1()
     [y Fs nbits] = wavread('piano_A4');
     Y = fft(y);
     n = length(Y);
+    t = 0:1:10000;
+    x = zeros(1,length(t));
     for i = 1:n/2
-    x = x+ abs(Y(i))*cos(2*pi*t*i);
+    x = x+ abs(Y(i))/n*cos(2*pi*t*i/n);
     end
     label = x;
 end
